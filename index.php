13257c10d83a39d5d0429b294f66b7c2433bab71
Romack Natividad <romacknatividad@gmail.com>
2024-10-08 15:51:03 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+require "vendor/autoload.php";
+require "init.php";
+
+// Database connection object (from init.php (DatabaseConnection))
+global $conn;
+
+try {
+
+    // Create Router instance
+    $router = new \Bramus\Router\Router();
+
+    // Define routes
+    $router->get('/', '\App\Controllers\HomeController@index');
+    $router->get('/suppliers', '\App\Controllers\SupplierController@list');
+    $router->get('/suppliers/{id}', '\App\Controllers\SupplierController@single');
+    $router->post('/suppliers/{id}', '\App\Controllers\SupplierController@update');
+
+    // Run it!
+    $router->run();
+
+} catch (Exception $e) {
+
+    echo json_encode([
+        'error' => $e->getMessage()
+    ]);
+
+}
